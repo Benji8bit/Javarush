fca48be2c8e42c9fc71ee16532afae401ef4010e
Benji8bit <coin8bit@hotmail.com>
2022-09-26 22:09:46 +0300

    Added Solution_4_14

--- src/main/java/Solution_4_14.java
+++ src/main/java/Solution_4_14.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Solution_4_14 {
+import java.util.Scanner;
+
+/* 
+Калибровка климата
+*/
+
+public class Solution_4_14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //напишите тут ваш код
+        int sum = 0;
+        for (int i =0; i<5; i++) {
+            sum+= scanner.nextInt();
+        }
+        int avg = sum/5;
+        System.out.println(avg);
+    }
 }
