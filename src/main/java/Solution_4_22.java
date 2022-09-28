f1b135562070a4488587333ea25767ac98005908
Benji8bit <coin8bit@hotmail.com>
2022-09-28 22:59:36 +0300

    Added Solution_4_22 - speed of wind

---
+++ src/main/java/Solution_4_22.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/* 
+Скорость ветра
+*/
+
+public class Solution_4_22 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Scanner console = new Scanner(System.in);
+        int speedms = console.nextInt();
+
+        double speedkmh = Math.round(3.6*speedms);
+
+        System.out.println((int)speedkmh);
+    }
+}
