49600f1d55b27567a4ffce3357b98d5ea38d588a
Benji8bit <coin8bit@hotmail.com>
2022-09-28 10:43:10 +0300

    Added Solution_4_17

---
+++ src/main/java/Solution_4_17.java
@@ -0,0 +1,30 @@
+/*
+Диггер (1)
+*/
+
+public class Solution_4_17 {
+    public static String bug = "🐛";
+    public static String robot = "🤖";
+    public static String land = "🌍";
+    public static String death = "💀";
+    public static String hole = " ";
+
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        for (int j=0; j<10; j++) {
+            System.out.println();
+            for (int i = 0; i < 10; i++) {
+                if ((j==0)&&(i==3)) {
+                    System.out.print(robot);
+                    continue;
+                }
+                if (j==9) {
+                    System.out.print(bug);
+                    continue;
+                }
+                System.out.print(land);
+            }
+        }
+    }
+}
