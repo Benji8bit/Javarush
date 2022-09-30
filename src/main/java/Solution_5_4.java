324a349f9ed3104f23635160595a3724d65dfe9b
Benji8bit <coin8bit@hotmail.com>
2022-09-30 21:38:17 +0300

    Added Solution_5_4

---
+++ src/main/java/Solution_5_4.java
@@ -0,0 +1,7 @@
+public class Solution_5_4 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int[] intArray = new int[10];
+        double[] doubleArray = new double[10];
+    }
+}
