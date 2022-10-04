066abceda1c35a062216128129de53cdf4b43397
Benji8bit <coin8bit@hotmail.com>
2022-10-04 23:09:38 +0300

    CyberDragon Hunt (5.9)

---
+++ src/main/java/Solution_5_9.java
@@ -0,0 +1,22 @@
+/*
+Взлом Кибердракона
+*/
+
+public class Solution_5_9 {
+    public static int[] coordinates;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        coordinates = new int[200];
+        int pairNumber = 100;
+        for (int i = 0; i<200; i++) {
+            if (i%2==0) {
+                coordinates[i] = i * 5;
+            }
+            else {
+                coordinates[i] = i * 5 - 5 + 1;
+            }
+            System.out.print(coordinates[i] + ", ");
+        }
+    }
+}
