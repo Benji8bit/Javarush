9f769afe20c527cf8e52116678a1e155477d26fa
Benji8bit <coin8bit@hotmail.com>
2022-10-07 07:10:37 +0300

    Added Solution_5_12 Find max value

---
+++ src/main/java/Solution_5_12.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/*
+Минимальное из N чисел
+*/
+
+public class Solution_5_12 {
+    public static int[] array;
+
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        Scanner console = new Scanner(System.in);
+        int num = console.nextInt();
+
+        array = new int[num];
+        for (int i = 0; i < num; i++) {
+            array[i] = console.nextInt();
+        }
+
+        int max = array[0];
+        for (int i = 1; i < num; i++) {
+            if (array[i] > max)
+                max = array[i];
+        }
+        System.out.println(max);
+    }
+}
