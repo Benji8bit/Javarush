0a0dc9ffcd1f703382069c81cb009bcc830c2c74
Benji8bit <coin8bit@hotmail.com>
2022-10-09 01:38:53 +0300

    Added Solution_5_14 Array initialization

---
+++ src/main/java/Solution_5_14.java
@@ -0,0 +1,20 @@
+/*
+Ловушка для Роботанка
+*/
+
+public class Solution_5_14 {
+    public static String[] array;
+
+    public static void main(String[] args) {
+        array = new String[]{"Amigo", "Diego", "Amigo", "Diego", "Amigo", "robotank"};
+
+        boolean isMissionComplete = false;
+        for (int i = 0; i < array.length; i++) {
+            if ("robotank".equalsIgnoreCase(array[i])) {
+                isMissionComplete = true;
+                break;
+            }
+        }
+        System.out.println(isMissionComplete ? "Mission complete" : "Mission not completed");
+    }
+}
