2d3bde4bd6a17ed0ee7c20879e6cdfffa90db8d5
Benji8bit <coin8bit@hotmail.com>
2022-10-09 02:19:43 +0300

    Added Solution_5_15 2-dim arrays - MULT Table

--- src/main/java/Solution_5_15.java
+++ src/main/java/Solution_5_15.java
@@ -1,17 +1,30 @@
-import java.util.Arrays;
+/*
+Таблица умножения
+*/
 
-class Main {
+public class Solution_5_15 {
+
+    public static int[][] MULTIPLICATION_TABLE;
+
     public static void main(String[] args) {
-        int[] array = {1, 5, 4, 3, 0}; //объявляем и инициализируем массив
-        System.out.println(array);//пытаемся вывести наш массив на экран без метода toString - получаем 16-ричное число
-        System.out.println(Arrays.toString(array));//печатаем массив "правильно"
-        Arrays.sort(array, 0, 4); //сортируем весь массив от нулевого до четвёртого члена
-        System.out.println(Arrays.toString(array));//выводим отсортированный массив на экран
-        int key = Arrays.binarySearch(array, 5); // ищем key - число 5 в отсортированном массиве.
-        //метод binarySearch выдаст индекс элемента остортированного массива, в котором "спрятано" искомое число
-        System.out.println(key);//распечатываем индекс искомого числа
-        System.out.println(Arrays.binarySearch(array, 0));//а теперь попробуем найти число, которого в массиве нет,
-        // и сразу же выведем результат на экран
+        //напишите тут ваш код
+        MULTIPLICATION_TABLE = new int[][]{{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+                {2, 4, 6, 8, 10, 12, 14, 16, 18, 20},
+                {3, 6, 9, 12, 15, 18, 21, 24, 27, 30},
+                {4, 8, 12, 16, 20, 24, 28, 32, 36, 40},
+                {5, 10, 15, 20, 25, 30, 35, 40, 45, 50},
+                {6, 12, 18, 24, 30, 36, 42, 48, 54, 60},
+                {7, 14, 21, 28, 35, 42, 49, 56, 63, 70},
+                {8, 16, 24, 32, 40, 48, 56, 64, 72, 80},
+                {9, 18, 27, 36, 45, 54, 63, 72, 81, 90},
+                {10, 20, 30, 40, 50, 60, 70, 80, 90, 100}};
 
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(MULTIPLICATION_TABLE[i][j] + " ");
+                if (j == 9) System.out.println();
+            }
+        }
     }
 }
