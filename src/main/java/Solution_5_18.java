7d1c9a47cbb865a20b110482f10b6c733d202c5d
Benji8bit <coin8bit@hotmail.com>
2022-10-10 12:36:37 +0300

    Added Solution_5_17 Triangle array

---
+++ src/main/java/Solution_5_18.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/*
+Создаем двумерный массив
+*/
+
+public class Solution_5_18 {
+    public static int[][] multiArray;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
