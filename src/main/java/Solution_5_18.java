43fd744b33cfdb871e8d4abb943435960749dec2
Benji8bit <coin8bit@hotmail.com>
2022-10-11 22:44:40 +0300

    Added Solution_5_19 Multiarray with any j

--- src/main/java/Solution_5_18.java
+++ src/main/java/Solution_5_18.java
@@ -9,5 +9,14 @@ public class Solution_5_18 {
 
     public static void main(String[] args) {
         //напишите тут ваш код
+
+        Scanner console = new Scanner(System.in);
+        int N = console.nextInt();
+
+        multiArray = new int[N][];
+
+        for (int i = 0; i < multiArray.length; i++) {
+            multiArray[i] = new int[console.nextInt()];
+        }
     }
 }
