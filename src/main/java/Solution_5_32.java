7db0295c4c0ff84200e87e44ed68b0b6c218b7d2
Benji8bit <coin8bit@hotmail.com>
2022-10-18 12:24:01 +0300

    Added Solution_5_33 Arrays.sort(int[]);

--- src/main/java/Solution_5_32.java
+++ src/main/java/Solution_5_32.java
@@ -4,12 +4,13 @@ import java.util.Arrays;
 Сортировка массива
 */
 
-public class Solution {
+public class Solution_5_32 {
 
     public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
 
     public static void main(String[] args) {
         //напишите тут ваш код
+        Arrays.sort(array);
         System.out.println(Arrays.toString(array));
     }
 }
