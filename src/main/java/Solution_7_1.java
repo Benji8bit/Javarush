efb8cdec49998aaad2d3014d8988383ee25f2777
Benji8bit <coin8bit@hotmail.com>
2022-10-20 18:45:14 +0300

    Added Solution_7_1 activateSword() method

---
+++ src/main/java/Solution_7_1.java
@@ -0,0 +1,21 @@
+/*
+Зачарованный меч
+*/
+
+public class Solution_7_1 {
+    public static void main(String[] args) {
+        System.out.println("Из болота лезут упыри и скелеты...");
+
+        System.out.println("Амиго активирует меч и легко уничтожает скелетов и упырей.");
+
+        activateSword();
+    }
+
+    //напишите тут ваш код
+    public static void activateSword() {
+        System.out.println("Амиго активирует меч и легко уничтожает скелетов и упырей.");
+    }
+
+
+
+}
