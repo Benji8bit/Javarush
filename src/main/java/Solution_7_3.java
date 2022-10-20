5179c48ca67970b29ded83eb037f7b42ddc5133d
Benji8bit <coin8bit@hotmail.com>
2022-10-20 19:25:50 +0300

    Added Solution_7_2 Methods divided

---
+++ src/main/java/Solution_7_3.java
@@ -0,0 +1,17 @@
+/*
+Упорядоченная информация
+*/
+
+public class Solution_7_3 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        print1();
+        print2();
+    }
+    public static void print1(){
+        System.out.println("Я — поэт, зовусь я Цветик.");
+    }
+    public static void print2(){
+        System.out.println("От меня вам всем приветик.");
+    }
+}
