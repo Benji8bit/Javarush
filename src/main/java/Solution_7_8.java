885f7f8abe0b676f7171819af4c249bb8c26a3e9
Benji8bit <coin8bit@hotmail.com>
2022-10-24 22:13:28 +0300

    Added Solution_7_8 Signature of method

---
+++ src/main/java/Solution_7_8.java
@@ -0,0 +1,50 @@
+/*
+Универсальный солдат
+*/
+
+public class Solution_7_8 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(int one){
+
+    }
+
+    public static void universalMethod(short two){
+
+    }
+
+    public static void universalMethod(long three){
+
+    }
+
+    public static void universalMethod(char four){
+
+    }
+
+    public static void universalMethod(String five){
+
+    }
+
+    public static void universalMethod(float six){
+
+    }
+
+    public static void universalMethod(double seven){
+
+    }
+
+    public static void universalMethod(boolean eight){
+
+    }
+
+    public static void universalMethod(int nine, double nineDotNine){
+
+    }
+
+    public static void universalMethod(byte zero){
+
+    }
+}
