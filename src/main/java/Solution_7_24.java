0cb75d205f270c5705effc547ee818cb6d1f7c02
Benji8bit <coin8bit@hotmail.com>
2022-11-04 14:59:09 +0300

    Added Solution_7_24 this implementation

---
+++ src/main/java/Solution_7_24.java
@@ -0,0 +1,17 @@
+/* 
+Учет работников
+*/
+
+public class Solution_7_24 {
+    public String name = "Amigo";
+    public String position = "Java developer";
+    public int salary = 10_000;
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
