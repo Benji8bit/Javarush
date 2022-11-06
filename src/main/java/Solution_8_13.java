cf5d26732f7ef337f2980521525382f36b58b7f1
Benji8bit <coin8bit@hotmail.com>
2022-11-06 16:14:39 +0300

    Fixed Solution_8_13 with Arrays.fill(4 args);

---
+++ src/main/java/Solution_8_13.java
@@ -0,0 +1,17 @@
+/*
+Гибкое заполнение массива
+*/
+
+import java.util.Arrays;
+
+public class Solution_8_13 {
+    public static void main(String[] args) {
+        Integer[] array = new Integer[5];
+        fillArray(array, 3, 1, 3);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void fillArray(Integer[] array, int value, int begin, int end) {
+        Arrays.fill(array, begin, end, value);
+    }
+}
