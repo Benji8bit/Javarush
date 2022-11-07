bafd5b897dff746b8c98f43c94a4fbd175f77510
Benji8bit <coin8bit@hotmail.com>
2022-11-07 12:51:18 +0300

    Created objects of classes

---
+++ src/main/java/Solution_8_14.java
@@ -0,0 +1,14 @@
+/*
+Космическая Робосутра
+*/
+
+public class Solution_8_14 {
+
+    public static void main(String[] args) {
+        Gun gun = new Gun();
+        RepairDrone drone = new RepairDrone();
+        RobotCleaner cleaner = new RobotCleaner();
+        AutomaticKitchen kitchen = new AutomaticKitchen();
+        ProtectiveFieldGenerator field = new ProtectiveFieldGenerator();
+    }
+}
