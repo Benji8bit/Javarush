892dba3ed90c6d520230ae68c496389214ef6ef8
Benji8bit <coin8bit@hotmail.com>
2022-11-08 18:15:44 +0300

    Created class Galavir with defend() and attack()

---
+++ src/main/java/GalavirXIII.java
@@ -0,0 +1,17 @@
+/*Атака на
+Зелёного кардинала*/
+
+public class GalavirXIII {
+
+    public static int superWeapon = Integer.MAX_VALUE;
+    public double health = Double.MAX_VALUE;
+
+    public void defend(double d) {
+        health -= (superWeapon / d);
+    }
+
+    public int attack() {
+        return superWeapon;
+    }
+
+}
