8775758de792e2bc62fbda899ebc4a8adb9fb071
Benji8bit <coin8bit@hotmail.com>
2022-11-09 22:22:49 +0300

    Created class BattleField

--- src/main/java/Solution_8_14.java
+++ src/main/java/Solution_8_14.java
@@ -5,10 +5,12 @@
 public class Solution_8_14 {
 
     public static void main(String[] args) {
-        Gun gun = new Gun();
+        /*Gun gun = new Gun();
         RepairDrone drone = new RepairDrone();
         RobotCleaner cleaner = new RobotCleaner();
         AutomaticKitchen kitchen = new AutomaticKitchen();
         ProtectiveFieldGenerator field = new ProtectiveFieldGenerator();
+*/
+        System.out.println(Integer.MAX_VALUE + Integer.MAX_VALUE);
     }
 }
