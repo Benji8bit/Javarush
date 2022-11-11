c252a2ae3f52537d8d9ba55dbca340dc88b0f038
Benji8bit <coin8bit@hotmail.com>
2022-11-12 02:42:22 +0300

    Added Solution_9_8

---
+++ src/main/java/Features_mod9/Solution_9_8.java
@@ -0,0 +1,14 @@
+package Features_mod9;
+
+/*
+Импорты: Part 1
+*/
+
+public class Solution_9_8 {
+    public static void main(java.lang.String[] args) {
+        java.util.ArrayList<java.lang.String> arrayList = new java.util.ArrayList<>();
+        java.io.BufferedReader bufferedReader = new java.io.BufferedReader(new java.io.StringReader(""));
+        java.math.BigInteger bigInteger = new java.math.BigInteger("123");
+    }
+}
+
