20dcc862264aeb9ee5291355225175e662d5806f
Benji8bit <coin8bit@hotmail.com>
2022-11-13 19:02:05 +0300

    Added Solution_9_10 Thread.sleep()

---
+++ src/main/java/Features_mod9/Solution_9_10.java
@@ -0,0 +1,22 @@
+package Features_mod9;
+
+/*
+Обратный отсчет
+*/
+
+public class Solution_9_10 {
+
+    public static void main(String[] args) throws InterruptedException {
+        countDown();
+    }
+    public static void countDown() throws InterruptedException {
+        System.out.println("3");
+        Thread.sleep(1000);
+        System.out.println("2");
+        Thread.sleep(1000);
+        System.out.println("1");
+        Thread.sleep(1000);
+        System.out.println("Старт!");
+    }
+}
+
