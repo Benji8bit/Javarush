84f381fa9bfd3136edea2d3ac6588526412c64f9
Benji8bit <coin8bit@hotmail.com>
2022-11-14 04:58:01 +0300

    Added Solution_9_11 Thread.sleep(millis, nanos)

---
+++ src/main/java/Features_mod9/Solution_9_11.java
@@ -0,0 +1,20 @@
+package Features_mod9;
+
+/*
+Counter
+
+*/
+public class Solution_9_11 {
+
+    public static void main(String[] args) throws InterruptedException {
+        printNumbers();
+    }
+
+    public static void printNumbers() throws InterruptedException {
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+            if (i == 10) break;
+            Thread.sleep(1, 300000);
+        }
+    }
+}
