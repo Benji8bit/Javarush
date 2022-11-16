a8bb54858f136c78046e76f0de1ebf2638527b04
Benji8bit <coin8bit@hotmail.com>
2022-11-16 18:25:47 +0300

    Added Solution_9_13

---
+++ src/main/java/Features_mod9/Solution_9_13.java
@@ -0,0 +1,18 @@
+package Features_mod9;
+
+/*
+Побитовые операции
+*/
+
+public class Solution_9_13 {
+
+    public static void main(String[] args) {
+        int x = 7;
+        //x = x & 25;
+        x = x & 5;
+        //x = x & 3;
+        x = x & 12;
+        x = x | 1;
+        System.out.println(x);
+    }
+}
