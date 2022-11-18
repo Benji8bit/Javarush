aa8475404defeb2adff9cfe851b0321fc009ed81
Benji8bit <coin8bit@hotmail.com>
2022-11-18 23:02:18 +0300

    Added Solution_10_1

---
+++ src/main/java/Mod10_Literals/Solution_10_1.java
@@ -0,0 +1,12 @@
+package Mod10_Literals;
+
+/*
+Целочисленные литералы
+*/
+
+public class Solution_10_1 {
+    public byte b = -128;
+    public short s = -32768;
+    public int i = 1_234_567_890;
+    public long l = 2_345_678_900L;
+}
