3aeb671cc640c2a73345a865086b3c952202f9ca
Benji8bit <coin8bit@hotmail.com>
2022-11-19 10:17:56 +0300

    Fixed Solution_10_1 + string literals

--- src/main/java/Mod10_Literals/Solution_10_1.java
+++ src/main/java/Mod10_Literals/Solution_10_1.java
@@ -9,4 +9,12 @@ public class Solution_10_1 {
     public short s = -32768;
     public int i = 1_234_567_890;
     public long l = 2_345_678_900L;
+
+
+    public String text = "Всегда пишите код так, "
+    + "будто сопровождать его будет "
+    + "склонный к насилию психопат, "
+    + "который знает, где вы живете. "
+    + "Martin Golding";
+
 }
