b85d1303eefad21ea2cadd1e7d8b5a49717b770d
Benji8bit <coin8bit@hotmail.com>
2022-11-19 10:24:22 +0300

    Fixed Solution_10_1 + char literals

--- src/main/java/Mod10_Literals/Solution_10_1.java
+++ src/main/java/Mod10_Literals/Solution_10_1.java
@@ -17,4 +17,16 @@ public class Solution_10_1 {
     + "который знает, где вы живете. "
     + "Martin Golding";
 
+    public static char first = '\u004A';
+    public static char second = 'a';
+    public static char third = '\u0076';
+    public static char fourth = 'a';
+
+    public static void main(String[] args) {
+        System.out.print(first);
+        System.out.print(second);
+        System.out.print(third);
+        System.out.print(fourth);
+    }
+
 }
