4b07ddbc6351b645d38dbebacddaf13308d585ac
Benji8bit <coin8bit@hotmail.com>
2022-11-19 08:59:01 +0300

    Added Solution_10_2 float literals

---
+++ src/main/java/Mod10_Literals/Solution_10_2.java
@@ -0,0 +1,15 @@
+package Mod10_Literals;
+
+/*
+Литералы с плавающей точкой
+*/
+
+public class Solution_10_2 {
+    public double a = 0.;
+    public double b = .0;
+    public double c = 100D;
+    public double d = 100.0;
+    public double e = 1.11E5;
+    public float f = 200F;
+    public float g = 0.F;
+}
