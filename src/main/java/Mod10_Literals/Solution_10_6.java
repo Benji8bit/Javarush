fd0633cb86de83e4bbeb3efa71b7f6c343e9fab2
Benji8bit <coin8bit@hotmail.com>
2022-11-21 22:25:44 +0300

    Added MathExample for Random class with map and treemap

---
+++ src/main/java/Mod10_Literals/Solution_10_6.java
@@ -0,0 +1,28 @@
+package Mod10_Literals;
+
+import java.util.regex.Pattern;
+
+/*
+Двоично-шестнадцатеричный конвертер
+*/
+
+public class Solution_10_6 {
+
+    public static void main(String[] args) {
+        String binaryNumber = "100111010000";
+        System.out.println("Двоичное число " + binaryNumber + " равно шестнадцатеричному числу " + toHex(binaryNumber));
+        String hexNumber = "9d0";
+        System.out.println("Шестнадцатеричное число " + hexNumber + " равно двоичному числу " + toBinary(hexNumber));
+    }
+
+    public static String toHex(String binaryNumber) {
+        //напишите тут ваш код
+        return null;
+    }
+
+    public static String toBinary(String hexNumber) {
+        //напишите тут ваш код
+        return null;
+    }
+}
+
