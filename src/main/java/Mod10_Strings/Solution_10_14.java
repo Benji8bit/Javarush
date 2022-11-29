bb51c73c0e76357a97ce7cd0bf9d28bf562012e3
Benji8bit <coin8bit@hotmail.com>
2022-11-29 23:24:14 +0300

    Added Solution_10_14 String[] strings = new String[st.countTokens()];

---
+++ src/main/java/Mod10_Strings/Solution_10_14.java
@@ -0,0 +1,29 @@
+package Mod10_Strings;
+
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/*
+StringTokenizer
+*/
+
+public class Solution_10_14 {
+    public static void main(String[] args) {
+        String packagePath = "java.util.stream";
+        String[] tokens = getTokens(packagePath, "\\.");
+        System.out.println(Arrays.toString(tokens));
+    }
+
+    public static String[] getTokens(String query, String delimiter) {
+        StringTokenizer st = new StringTokenizer(query, delimiter);
+        int i = 0;
+        String[] strings = new String[st.countTokens()];
+        while (st.hasMoreTokens()) {
+            String token = st.nextToken();
+            strings[i] = token;
+            i++;
+        }
+        return  strings;
+    }
+}
+
