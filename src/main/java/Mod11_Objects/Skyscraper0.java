3f16ad19c1d0710f287952e8dccb683e893dfb61
Benji8bit <coin8bit@hotmail.com>
2022-12-03 21:57:47 +0300

    Added Skyscraper classes with constructors

---
+++ src/main/java/Mod11_Objects/Skyscraper0.java
@@ -0,0 +1,16 @@
+package Mod11_Objects;
+
+/*
+Создадим небоскреб
+*/
+
+public class Skyscraper0 {
+
+    public Skyscraper0() {
+        System.out.println("Небоскреб построен.");
+    }
+
+    public static void main(String[] args) {
+        Skyscraper0 skyscraper = new Skyscraper0();
+    }
+}
