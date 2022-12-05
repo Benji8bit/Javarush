9f3b66c91bb7064ddaf6b8d81cefdeadc289b006
Benji8bit <coin8bit@hotmail.com>
2022-12-05 22:56:05 +0300

    Added TerribleTwoBit class public TerribleTwoBit() { this.mission = "быстрые и комфортные перевозки";                                }

---
+++ src/main/java/Mod11_Objects/Solution_11_3.java
@@ -0,0 +1,8 @@
+package Mod11_Objects;
+
+public class Solution_11_3 {
+
+    public static void main(String[] args) {
+        System.out.println(new TerribleTwoBit().getMission());
+    }
+}
---
+++ src/main/java/Mod11_Objects/TerribleTwoBit.java
@@ -0,0 +1,13 @@
+package Mod11_Objects;
+
+public class TerribleTwoBit {
+    private String mission = "разрушать и разорять";
+
+    public String getMission() {
+        return "Моя миссия: " + mission;
+    }
+
+    public TerribleTwoBit() {
+        this.mission = "быстрые и комфортные перевозки";
+    }
+}
