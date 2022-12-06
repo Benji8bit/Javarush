2847eebcf6c69e7ee83ef5cafdd3759b08b96cb8
Benji8bit <coin8bit@hotmail.com>
2022-12-06 23:20:33 +0300

    Added Bugatti class with getter and setter

---
+++ src/main/java/Mod11_Objects/Bugatti.java
@@ -0,0 +1,24 @@
+package Mod11_Objects;
+
+/*
+С крышей или без? Вот в чем вопрос
+*/
+
+public class Bugatti {
+    private String color = "BLACK";
+    private int year = 2020;
+    private String body = "Coupe";
+
+    public Bugatti() {
+
+    }
+
+    public String getBody() {
+        return this.body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+}
