d04c89b136ba5af49e8cecabfd1f95df9630c112
Benji8bit <coin8bit@hotmail.com>
2022-12-12 00:15:46 +0300

    Added Solution_12_6 class about static and non-static methods and this keyword

---
+++ src/main/java/Mod12_Classes/Solution_12_6.java
@@ -0,0 +1,14 @@
+package Mod12_Classes;
+
+/*
+Оконные работы
+*/
+
+public class Solution_12_6 {
+    public static void main(String[] args) {
+        Window window = new Window();
+        window.printSize();
+        window.changeSize(4, 5);
+        window.printSize();
+    }
+}
