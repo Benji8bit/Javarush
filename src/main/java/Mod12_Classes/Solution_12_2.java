270709052c83517b6e5706034512163b8f5869a0
Benji8bit <coin8bit@hotmail.com>
2022-12-11 23:21:22 +0300

    Added Earth class about static and non-static fields

---
+++ src/main/java/Mod12_Classes/Solution_12_2.java
@@ -0,0 +1,17 @@
+package Mod12_Classes;
+
+/*
+Земля: техническая характеристика
+*/
+
+public class Solution_12_2 {
+
+    public static void main(String[] args) {
+        Planet earth = new Planet();
+        earth.name = "Земля";
+        earth.age = 4_540_000_000L;
+        earth.speed = 170_218;
+        earth.area = 510_072_000;
+        earth.printInformation();
+    }
+}
