a3e55f415b94dba32668affbf58ec6578229bed4
Benji8bit <coin8bit@hotmail.com>
2022-12-12 00:05:11 +0300

    Added Solution_12_5 class about static and non-static methods

---
+++ src/main/java/Mod12_Classes/Solution_12_5.java
@@ -0,0 +1,13 @@
+package Mod12_Classes;
+
+/*
+Конвертер строки
+*/
+
+public class Solution_12_5 {
+    public static void main(String[] args) {
+        String string = "12.84";
+        System.out.println(Math.round(Double.parseDouble(string)));
+    }
+}
+
