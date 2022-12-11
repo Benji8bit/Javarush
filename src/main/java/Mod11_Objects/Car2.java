0b9d3d2ca3acbbc1f2114d3e0a3620f9f6c7fb34
Benji8bit <coin8bit@hotmail.com>
2022-12-11 19:20:57 +0300

    Added Car2 class about initialize() method

---
+++ src/main/java/Mod11_Objects/Car2.java
@@ -0,0 +1,18 @@
+package Mod11_Objects;
+
+/*
+Автопроизводитель
+*/
+
+public class Car2 {
+    private String model;
+    private int year;
+    private String color;
+
+    public void initialize(String model, int year, String color) {
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+
+}
