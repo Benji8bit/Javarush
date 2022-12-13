8748817416b8024285be8f08720286506c823f40
Benji8bit <coin8bit@hotmail.com>
2022-12-13 21:38:33 +0300

    Added Solution_12_7 class about static class Calculator

---
+++ src/main/java/Mod12_Classes/Solution_12_7.java
@@ -0,0 +1,36 @@
+package Mod12_Classes;
+
+/*
+Утильный калькулятор
+*/
+
+public class Solution_12_7 {
+    public static final String EQUAL = " = ";
+
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 15;
+        Calculator.add(a, b);
+        Calculator.subtract(a, b);
+        Calculator.multiply(a, b);
+        Calculator.divide(a, b);
+    }
+
+    public static class Calculator {
+        public static void add(int a, int b) {
+            System.out.println(a + " + " + b + EQUAL + (a + b));
+        }
+
+        public static void subtract(int a, int b) {
+            System.out.println(a + " - " + b + EQUAL + (a - b));
+        }
+
+        public static void multiply(int a, int b) {
+            System.out.println(a + " * " + b + EQUAL + (a * b));
+        }
+
+        public static void divide(int a, int b) {
+            System.out.println(a + " / " + b + EQUAL + (a / b));
+        }
+    }
+}
