5413b63670b1f6b10d0d2fa5bdaa5b1c8a002fed
Benji8bit <coin8bit@hotmail.com>
2022-12-16 14:32:32 +0300

    Added Solution_12_9 about semicolons

---
+++ src/main/java/Mod12_Classes/Solution_12_9.java
@@ -0,0 +1,13 @@
+package Mod12_Classes;
+
+/*
+Точка и запятая
+*/
+
+public class Solution_12_9 {
+    public static void main(String[] args) {
+        int number = 1;
+        System.out.println(number);
+    }
+}
+
