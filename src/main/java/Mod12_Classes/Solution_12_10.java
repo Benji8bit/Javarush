8c1390d3c74513c24e1b893335d85a813ac7d90e
Benji8bit <coin8bit@hotmail.com>
2022-12-17 15:30:58 +0300

    Added Solution_12_10 about matches

---
+++ src/main/java/Mod12_Classes/Solution_12_10.java
@@ -0,0 +1,13 @@
+package Mod12_Classes;
+
+/*
+Ох уж эти кавычки
+*/
+
+public class Solution_12_10 {
+    public static void main(String[] args) {
+        String string = "Amigo";
+        System.out.println(string);
+    }
+}
+
