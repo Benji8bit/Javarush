187297f292fbfef42199f24310aac8f16bab382b
Benji8bit <coin8bit@hotmail.com>
2022-12-18 13:24:24 +0300

    Added Solution_12_11 about concatenation

---
+++ src/main/java/Mod12_Classes/Solution_12_11.java
@@ -0,0 +1,13 @@
+package Mod12_Classes;
+
+/*
+Кон, котэ и нация
+*/
+
+public class Solution_12_11 {
+    public static void main(String[] args) {
+        String name = "Diego";
+        int age = 35;
+        System.out.println("Name : " + name + " Age : " + age);
+    }
+}
