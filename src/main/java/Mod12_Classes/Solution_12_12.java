167c49bec788f48fb06f12d66c1cca84649d24bb
Benji8bit <coin8bit@hotmail.com>
2022-12-19 10:08:26 +0300

    Added Solution_12_12 about curly braces

---
+++ src/main/java/Mod12_Classes/Solution_12_12.java
@@ -0,0 +1,13 @@
+package Mod12_Classes;
+
+/*
+Следим за скобками
+*/
+
+public class Solution_12_12 {
+    public static void main(String[] args) {
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            System.out.println("Windows detected!");
+        }
+    }
+}
