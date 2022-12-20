9b4e41b0728e6b02ce90ef5527b1d98d5c77ff70
Benji8bit <coin8bit@hotmail.com>
2022-12-20 17:26:17 +0300

    Added Solution_12_12 about braces

---
+++ src/main/java/Mod12_Classes/Solution_12_13.java
@@ -0,0 +1,17 @@
+package Mod12_Classes;
+
+/*
+И снова скобки
+*/
+
+public class Solution_12_13 {
+    public static void main(String[] args) {
+        int number = 1;
+        if (number < 5) {
+            System.out.println("number < 5");
+        } else {
+            System.out.println();
+        }
+    }
+}
+
