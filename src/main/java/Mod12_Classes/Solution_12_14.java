cd819e133a7086a3d8aec1b788827ee51019b31f
Benji8bit <coin8bit@hotmail.com>
2022-12-21 12:59:39 +0300

    Added Solution_12_12 about main method

---
+++ src/main/java/Mod12_Classes/Solution_12_14.java
@@ -0,0 +1,12 @@
+package Mod12_Classes;
+
+/*
+Многострадальный метод main
+*/
+
+public class Solution_12_14 {
+    public static void main(String... errors) {
+        System.out.println("It works!");
+    }
+}
+
