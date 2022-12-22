227a100c94cbcf3a05f3d67019f9922263c80b9b
Benji8bit <coin8bit@hotmail.com>
2022-12-22 03:57:43 +0300

    Added Solution_12_15 about class names

---
+++ src/main/java/Mod12_Classes/Solution_12_15.java
@@ -0,0 +1,15 @@
+package Mod12_Classes;
+
+/*
+Правильный класс
+*/
+
+public class Solution_12_15 {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        System.out.println(cat.name);
+    }
+}
+class Cat {
+    String name = "Tom";
+}
