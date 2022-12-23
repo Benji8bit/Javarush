adcc9488071bbef9853b9807222136f1164df8fb
Benji8bit <coin8bit@hotmail.com>
2022-12-23 12:30:22 +0300

    Added Solution_12_16 about imports

---
+++ src/main/java/Mod12_Classes/Solution_12_16.java
@@ -0,0 +1,17 @@
+package Mod12_Classes;
+
+/*
+Импортный код
+*/
+
+import java.util.Scanner;
+
+public class Solution_12_16 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        if (console.hasNext()) {
+            System.out.println(console.next());
+        }
+    }
+}
+
