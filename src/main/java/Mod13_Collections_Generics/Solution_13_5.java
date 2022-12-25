a29ea483dd6f88d437e0147e4e6f291391038cb9
Benji8bit <coin8bit@hotmail.com>
2022-12-25 20:03:44 +0300

    Added Solution_13_5 class about NaN and Infinity

--- src/main/java/Mod13_Collections_Generics/Solution_13_5.java
+++ src/main/java/Mod13_Collections_Generics/Solution_13_5.java
@@ -15,33 +15,18 @@ public class Solution_13_5 {
     }
 
     public static void divide(double a, double b) {
-        if (((a > 0)&&(b == 0))
-                ||((a == Double.POSITIVE_INFINITY)&&(b >= 0)&&(b != Double.POSITIVE_INFINITY))
-                ||((a == Double.NEGATIVE_INFINITY)&&(b < 0)&&(b != Double.NEGATIVE_INFINITY)))
-        {
+        if (((a > 0) && (b == 0))
+                || ((a == Double.POSITIVE_INFINITY) && (b >= 0) && (b != Double.POSITIVE_INFINITY))
+                || ((a == Double.NEGATIVE_INFINITY) && (b < 0) && (b != Double.NEGATIVE_INFINITY))) {
             System.out.println(Double.POSITIVE_INFINITY);
-        }
-
-        else if (((a < 0)&&(b == 0))
-                ||((a == Double.NEGATIVE_INFINITY)&&(b >= 0)&&(b != Double.POSITIVE_INFINITY))
-                ||((a == Double.POSITIVE_INFINITY)&&(b < 0)&&(b != Double.NEGATIVE_INFINITY)))
-        {
+        } else if (((a < 0) && (b == 0))
+                || ((a == Double.NEGATIVE_INFINITY) && (b >= 0) && (b != Double.POSITIVE_INFINITY))
+                || ((a == Double.POSITIVE_INFINITY) && (b < 0) && (b != Double.NEGATIVE_INFINITY))) {
             System.out.println(Double.NEGATIVE_INFINITY);
-        }
-
-        else if (((a == 0)&&(b == 0))
-                || (Double.isNaN(a))
-                || (Double.isNaN(b))
-                ||((a == Double.POSITIVE_INFINITY)&&(b == Double.POSITIVE_INFINITY))
-                ||((a == Double.POSITIVE_INFINITY)&&(b == Double.NEGATIVE_INFINITY))
-                ||((a == Double.NEGATIVE_INFINITY)&&(b == Double.POSITIVE_INFINITY))
-                || ((a == Double.NEGATIVE_INFINITY)&&(b == Double.NEGATIVE_INFINITY))               )
-        {
+        } else if (a == 0 && b == 0 || Double.isNaN(a) || Double.isNaN(b) || a == Double.POSITIVE_INFINITY && b == Double.POSITIVE_INFINITY || a == Double.POSITIVE_INFINITY && b == Double.NEGATIVE_INFINITY || a == Double.NEGATIVE_INFINITY && b == Double.POSITIVE_INFINITY || a == Double.NEGATIVE_INFINITY && b == Double.NEGATIVE_INFINITY) {
             System.out.println(Double.NaN);
-        }
-
-        else {
-            System.out.println(a/b);
+        } else {
+            System.out.println(a / b);
         }
     }
 }
