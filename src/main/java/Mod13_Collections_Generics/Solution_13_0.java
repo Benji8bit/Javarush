2580cf163d0a636eab94b453015286487e34dc99
Benji8bit <coin8bit@hotmail.com>
2022-12-25 17:46:16 +0300

    Added Solution_13_0 class about ValueOf() method

---
+++ src/main/java/Mod13_Collections_Generics/Solution_13_0.java
@@ -0,0 +1,27 @@
+package Mod13_Collections_Generics;
+
+/*
+Автоупаковка
+*/
+
+public class Solution_13_0 {
+    byte byteValue;
+    short shortValue;
+    int intValue;
+    long longValue;
+
+    float floatValue;
+    double doubleValue;
+
+    char charValue;
+    boolean booleanValue;
+
+    Byte byteValueBox = Byte.valueOf(byteValue);
+    Short shortValueBox = Short.valueOf(shortValue);
+    Integer integerValueBox = Integer.valueOf(intValue);
+    Long longValueBox = Long.valueOf(longValue);
+    Float floatValueBox = Float.valueOf(floatValue);
+    Double doubleValueBox = Double.valueOf(doubleValue);
+    Character charValueBox = Character.valueOf(charValue);
+    Boolean booleanValueBox = Boolean.valueOf(booleanValue);
+}
