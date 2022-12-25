6af0c615c28d19e2a3bf91e4b7246ce71dc96c74
Benji8bit <coin8bit@hotmail.com>
2022-12-25 16:07:04 +0300

    Added Solution_12_21 class about constructor

---
+++ src/main/java/Mod12_Classes/Solution_12_21.java
@@ -0,0 +1,12 @@
+package Mod12_Classes;
+
+/*
+Студент
+*/
+
+public class Solution_12_21 {
+    public static void main(String[] args) {
+        Student student = new Student("Амиго");
+        System.out.println(student.name);
+    }
+}
---
+++ src/main/java/Mod12_Classes/Student.java
@@ -0,0 +1,13 @@
+package Mod12_Classes;
+
+/*
+Студент
+*/
+
+public class Student {
+    String name;
+
+    Student(String name) {
+        this.name = name;
+    }
+}
