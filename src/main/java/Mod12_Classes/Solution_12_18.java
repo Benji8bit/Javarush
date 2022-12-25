f3cffc9efcce9153a9ce77dad7647be504c67f10
Benji8bit <coin8bit@hotmail.com>
2022-12-25 15:30:39 +0300

    Added Solution_12_18 class about immutability

---
+++ src/main/java/Mod12_Classes/Solution_12_18.java
@@ -0,0 +1,17 @@
+package Mod12_Classes;
+
+import java.util.Scanner;
+
+/*
+Все буквы маленькие
+*/
+
+public class Solution_12_18 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String line = console.nextLine();
+        line = line.toLowerCase();
+        System.out.println(line);
+    }
+}
+
