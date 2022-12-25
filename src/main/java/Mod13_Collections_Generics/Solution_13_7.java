823eba3b7e88342c55413e139ab2939f68d5a680
Benji8bit <coin8bit@hotmail.com>
2022-12-25 20:28:18 +0300

    Added Solution_13_7 class about Boolean

---
+++ src/main/java/Mod13_Collections_Generics/Solution_13_7.java
@@ -0,0 +1,24 @@
+package Mod13_Collections_Generics;
+
+/*
+Какое число меньше
+*/
+
+public class Solution_13_7 {
+    public static void main(String[] args) {
+        int first = 2;
+        int second = 1;
+
+        Boolean isLess = isLess(first, second);
+
+        if (isLess) {
+            System.out.println("Первое число меньше второго");
+        } else {
+            System.out.println("Первое число больше или равно второму числу");
+        }
+    }
+
+    public static Boolean isLess(int first, int second) {
+        return first < second ? true : false;
+    }
+}
