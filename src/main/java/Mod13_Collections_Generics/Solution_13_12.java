e1276495dfa780310d99ec4ee04aee009eac7e94
Benji8bit <coin8bit@hotmail.com>
2022-12-28 22:01:44 +0300

    Added Solution_13_12 about ArrayList get, set and size() methods

---
+++ src/main/java/Mod13_Collections_Generics/Solution_13_12.java
@@ -0,0 +1,40 @@
+package Mod13_Collections_Generics;
+
+/*
+Переворачивание данных
+*/
+
+import java.util.ArrayList;
+
+public class Solution_13_12 {
+    public static ArrayList<Integer> numbers = new ArrayList<>();
+
+    public static void main(String[] args) {
+        init();
+        print();
+
+        reverse();
+        print();
+    }
+
+    public static void init() {
+        for (int i = 0; i < 10; i++) {
+            numbers.add(i);
+        }
+    }
+
+    public static void reverse() {
+        int n = numbers.size() - 1;
+        for (int i = 0; i < numbers.size() / 2; i++) {
+            int temp = numbers.get(i);
+            numbers.set(i, n - i);
+            numbers.set(n - i, temp);
+        }
+    }
+
+    private static void print() {
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
