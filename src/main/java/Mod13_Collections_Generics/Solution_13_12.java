f38da40ffec0564172075bd5811548121d8493ec
Benji8bit <coin8bit@hotmail.com>
2022-12-29 01:26:17 +0300

    Fixed Solution_13_12 about ArrayList get, set and size() methods

--- src/main/java/Mod13_Collections_Generics/Solution_13_12.java
+++ src/main/java/Mod13_Collections_Generics/Solution_13_12.java
@@ -27,7 +27,7 @@ public class Solution_13_12 {
         int n = numbers.size() - 1;
         for (int i = 0; i < numbers.size() / 2; i++) {
             int temp = numbers.get(i);
-            numbers.set(i, n - i);
+            numbers.set(i, numbers.get(n - i));
             numbers.set(n - i, temp);
         }
     }
