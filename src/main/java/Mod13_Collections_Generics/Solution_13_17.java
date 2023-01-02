8f48b23836afbf6fe310bc646b6872bd33f3e636
Benji8bit <coin8bit@hotmail.com>
2023-01-02 22:05:47 +0300

    Added Solution_13_17 about ArrayList add(int index, String name) method

---
+++ src/main/java/Mod13_Collections_Generics/Solution_13_17.java
@@ -0,0 +1,41 @@
+package Mod13_Collections_Generics;
+
+import java.util.ArrayList;
+
+/*
+Солнечная система
+*/
+
+public class Solution_13_17 {
+    public static ArrayList<String> planets = new ArrayList<>();
+
+    public static void main(String[] args) {
+        addPlanets();
+        print();
+        createNewPlanet("Звезда Смерти");
+        print();
+    }
+
+    public static void createNewPlanet(String planetName) {
+        planets.add(3, planetName);
+    }
+
+    public static void addPlanets() {
+        planets.add("Меркурий");
+        planets.add("Венера");
+        planets.add("Земля");
+        planets.add("Марс");
+        planets.add("Юпитер");
+        planets.add("Сатурн");
+        planets.add("Уран");
+        planets.add("Нептун");
+    }
+
+    public static void print() {
+        for (int i = 0; i < planets.size(); i++) {
+            System.out.println(String.format("%s — %d-я планета от Солнца", planets.get(i), (i + 1)));
+        }
+        System.out.println();
+    }
+}
+
