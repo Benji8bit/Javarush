98fb644dbc05aa40ac5351e704a4305137e0630b
Benji8bit <coin8bit@hotmail.com>
2023-01-15 21:26:31 +0300

    Added Solution_13_21 about var keyword

---
+++ src/main/java/Mod13_Collections_Generics/Solution_13_21.java
@@ -0,0 +1,24 @@
+package Mod13_Collections_Generics;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/*
+Вперед в будущее
+*/
+
+public class Solution_13_21 {
+    private int number = 54;
+    private String string = "string";
+
+    public static void main(String[] args) {
+        var integer = 22;
+        var string = "string";
+        var array = new int[5];
+        var strings = new ArrayList<String>();
+        var bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        var maxValue = Integer.valueOf(Integer.MAX_VALUE);
+    }
+}
+
