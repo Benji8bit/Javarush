cb2dfbffb7115f02c3d50338e9faf590f804dbc3
Benji8bit <coin8bit@hotmail.com>
2023-01-16 22:45:13 +0300

    Added Solution_13_22 about var keyword

---
+++ src/main/java/Mod13_Collections_Generics/Solution_13_22.java
@@ -0,0 +1,24 @@
+package Mod13_Collections_Generics;
+
+/*
+Назад в прошлое
+*/
+
+public class Solution_13_22 {
+    private int[] intArray = new int[4];
+
+    public int[] getIntArray() {
+        return intArray;
+    }
+
+    public void setIntArray(int[] intArray) {
+        this.intArray = intArray;
+    }
+
+    public static void main(String[] args) {
+        Solution_13_22 solution = new Solution_13_22();
+        String string = new String("Hello");
+        StringBuilder stringBuilder = new StringBuilder();
+    }
+}
+
