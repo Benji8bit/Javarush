cdd062d08da321e2663abad6c7f06d3b43968468
Benji8bit <coin8bit@hotmail.com>
2023-01-17 12:26:25 +0300

    Added Solution_13_23 about var and <> diamond

---
+++ src/main/java/Mod13_Collections_Generics/Solution_13_23.java
@@ -0,0 +1,20 @@
+package Mod13_Collections_Generics;
+
+import java.io.File;
+import java.util.*;
+
+/*
+Shine bright like a diamond
+*/
+
+public class Solution_13_23 {
+
+    public static void main(String[] args) {
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+        Map<String, ArrayList<String>> map = new HashMap<>();
+        var exceptionsList = new ArrayList<Exception>();
+        var filesStack = new Stack<File>();
+    }
+}
+
