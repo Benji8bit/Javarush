ca054218419c8a50f9d24401b91dafd667e109a2
Benji8bit <coin8bit@hotmail.com>
2023-02-12 00:47:36 +0300

    Added Solution_14_16 about LinkedList method add()

--- src/main/java/Mod14_Collections/StringsLinkedList.java
+++ src/main/java/Mod14_Collections/StringsLinkedList.java
@@ -12,8 +12,28 @@ public class StringsLinkedList {
         }
     }
 
+/*1) Создаем Node node, записываем туда value.
+  2) Смотрим, какой элемент был до этого последним
+  (на него ссылается last.prev).
+  Его записываем в node.prev.
+  И наоборот: в next этого элемента пишем node.
+  Если last.prev==null (то есть до этого список был пустой),
+  то эти манипуляции проделываем с first.
+  3) В last.prev пишем ссылку на node,
+  и наоборот: в node.next пишем ссылку на last.
+ */
     public void add(String value) {
-        //напишите тут ваш код
+        Node element = new Node();
+        element.value = value;
+
+        if (first.next == null) {
+            first.next = element;
+        }
+        else {
+            last.prev.next = element;
+            element.prev = last.prev;
+        }
+        last.prev = element;
     }
 
     public static class Node {
