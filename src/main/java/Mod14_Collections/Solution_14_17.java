a1a5692b8340181d2c4e20452d2082d35e8a47fc
Benji8bit <coin8bit@hotmail.com>
2023-02-14 21:18:23 +0300

    Added Solution_14_17 about LinkedList method get()

---
+++ src/main/java/Mod14_Collections/Solution_14_17.java
@@ -0,0 +1,23 @@
+package Mod14_Collections;
+
+/*
+StringLinkedList, часть 2
+*/
+
+public class Solution_14_17 {
+
+    public static void main(String[] args) {
+        StringLinkedList stringLinkedList = new StringLinkedList();
+        stringLinkedList.add("1");
+        stringLinkedList.add("2");
+        stringLinkedList.add("3");
+        stringLinkedList.add("4");
+        stringLinkedList.add("5");
+        stringLinkedList.add("6");
+        stringLinkedList.add("7");
+        stringLinkedList.add("8");
+        stringLinkedList.add("9");
+        System.out.println(stringLinkedList.get(4)); // 5
+    }
+}
+
