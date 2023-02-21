c9221484dc5819d86e193585e0e83466b341ed9a
Benji8bit <coin8bit@hotmail.com>
2023-02-21 21:59:49 +0300

    Added Solution_14_20 and enum JavarushQuest about Enum methods values() and ordinal()

---
+++ src/main/java/Mod14_Collections/Month.java
@@ -0,0 +1,21 @@
+package Mod14_Collections;
+
+/*
+Трансформируем enum в класс
+*/
+
+public enum Month {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+}
+
