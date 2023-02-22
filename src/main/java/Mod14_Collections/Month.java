6b6c48c8a741c9be3d2bddb7006ba384e9853368
Benji8bit <coin8bit@hotmail.com>
2023-02-22 19:02:52 +0300

    Added class Month about Enum methods

--- src/main/java/Mod14_Collections/Month.java
+++ src/main/java/Mod14_Collections/Month.java
@@ -1,21 +1,25 @@
 package Mod14_Collections;
 
-/*
+public class Month {
+
+    /*
 Трансформируем enum в класс
 */
 
-public enum Month {
-    JANUARY,
-    FEBRUARY,
-    MARCH,
-    APRIL,
-    MAY,
-    JUNE,
-    JULY,
-    AUGUST,
-    SEPTEMBER,
-    OCTOBER,
-    NOVEMBER,
-    DECEMBER
-}
+    public enum Month {
+        JANUARY,
+        FEBRUARY,
+        MARCH,
+        APRIL,
+        MAY,
+        JUNE,
+        JULY,
+        AUGUST,
+        SEPTEMBER,
+        OCTOBER,
+        NOVEMBER,
+        DECEMBER
+    }
 
+
+}
