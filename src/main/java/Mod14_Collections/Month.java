1de13194e4d0242cd87734cbd9162d6cb9797a4c
Benji8bit <coin8bit@hotmail.com>
2023-02-24 20:52:47 +0300

    Update Month.class

--- src/main/java/Mod14_Collections/Month.java
+++ src/main/java/Mod14_Collections/Month.java
@@ -5,20 +5,18 @@ package Mod14_Collections;
 */
 
 public class Month {
-    
-    private int value;
-    
-    private Month (int value)
-  {
-    this.value = value;
-  }
-    
-    public int ordinal()
-  {
-    return this.value;
-  }
 
-   
+    private int value;
+
+    private Month(int value) {
+
+        this.value = value;
+    }
+
+    public int ordinal() {
+        return this.value;
+    }
+
     public static final Month JANUARY = new Month(0);
     public static final Month FEBRUARY = new Month(1);
     public static final Month MARCH = new Month(2);
@@ -30,14 +28,14 @@ public class Month {
     public static final Month SEPTEMBER = new Month(8);
     public static final Month OCTOBER = new Month(9);
     public static final Month NOVEMBER = new Month(10);
-    public static final Month DECEMBER = new Month(11); 
-    
+    public static final Month DECEMBER = new Month(11);
+
     private static final Month[] array = {JANUARY, FEBRUARY,
-      MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER};
-    
-    public static Month[] values()
-   {
-      return array;
-   } 
+            MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER};
+
+    public static Month[] values() {
+
+        return array;
+    }
 
 }
