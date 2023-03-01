90c2e0f549a816afda3fca5f58c05219032a44eb
Benji8bit <coin8bit@hotmail.com>
2023-03-01 21:38:21 +0300

    Added Solution_14_22 and Error public enum

---
+++ src/main/java/Mod14_Collections/Solution_14_22.java
@@ -0,0 +1,13 @@
+package Mod14_Collections;
+
+/*
+Ошибка выхода из анабиоза
+*/
+
+public class Solution_14_22 {
+
+    public static void main(String[] args) {
+        Error error = Error.getErrorByID(200);
+        System.out.println(error);
+    }
+}
