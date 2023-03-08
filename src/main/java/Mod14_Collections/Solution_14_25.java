7f6ed953b9d5739af8ee3f6dec13b1acae1381ed
Benji8bit <coin8bit@hotmail.com>
2023-03-08 21:55:36 +0300

    Added Solution_14_25 about switch-case-default operators

---
+++ src/main/java/Mod14_Collections/Solution_14_25.java
@@ -0,0 +1,31 @@
+package Mod14_Collections;
+
+public class Solution_14_25 {
+
+    public static void main(String[] args) {
+        System.out.println(getShapeNameByCountOfCorners(3));
+        System.out.println(getShapeNameByCountOfCorners(5));
+        System.out.println(getShapeNameByCountOfCorners(1));
+    }
+
+    public static String getShapeNameByCountOfCorners(int countOfCorner) {
+        String shape;
+        if (countOfCorner == 3) {
+            shape = "Треугольник";
+        } else if (countOfCorner == 4) {
+            shape = "Четырехугольник";
+        } else if (countOfCorner == 5) {
+            shape = "Пятиугольник";
+        } else if (countOfCorner == 6) {
+            shape = "Шестиугольник";
+        } else if (countOfCorner == 7) {
+            shape = "Семиугольник";
+        } else if (countOfCorner == 8) {
+            shape = "Восьмиугольник";
+        } else {
+            shape = "Другая фигура";
+        }
+        return shape;
+    }
+}
+
