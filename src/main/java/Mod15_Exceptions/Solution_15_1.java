7c14ced6af808314648668ea953e8520c91b638c
Benji8bit <coin8bit@hotmail.com>
2023-03-16 21:26:22 +0300

    Added Solution_15_1 about try-catch block

---
+++ src/main/java/Mod15_Exceptions/Solution_15_1.java
@@ -0,0 +1,45 @@
+package Mod15_Exceptions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/*
+Помощник преподавателя
+*/
+
+public class Solution_15_1 {
+
+    public static final String PROMPT_STRING = "Введите номер студента, или exit для выхода: ";
+    public static final String EXIT = "exit";
+    public static final String ANSWERING = "Отвечает ";
+    public static final String NOT_EXIST = "Студента с таким номером не существует";
+
+    static List<String> studentsJournal = Arrays.asList(
+            "Тимур Мясной"
+            , "Пенелопа Сиволап"
+            , "Орест Злобин"
+            , "Ирида Продувалова"
+            , "Гектор Гадюкин"
+            , "Электра Чемоданова"
+            , "Гвидон Недумов"
+            , "Роксана Борисенко"
+            , "Юлиан Мумбриков"
+            , "Зигфрид Горемыкин");
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print(PROMPT_STRING);
+            String input = scanner.nextLine();
+            if (input.toLowerCase().equals(EXIT)) {
+                break;
+            }
+
+            int studentId = Integer.parseInt(input);
+            System.out.println(ANSWERING + studentsJournal.get(studentId));
+        }
+    }
+}
+
